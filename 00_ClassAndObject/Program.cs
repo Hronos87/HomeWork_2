fc022a61a766ba67c3291222d5f50dcda5a1bbc5
Hronos87 <98458802+Hronos87@users.noreply.github.com>
2022-02-08 19:39:32 +0300

    Add project files.

---
+++ 00_ClassAndObject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Less01_task01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyClass instance = new MyClass();
+			instance.field = "myInstance";
+			Console.WriteLine(instance.field);
+		}
+	}
+
+	class MyClass
+	{
+		public string field;
+	}
+}
---
+++ 01_ClassAndPoketOOB/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01_ClassAndPoketOOB
+{
+    class MyClass
+    {
+        public int field;
+
+        public void Method()
+        {
+            Console.WriteLine(field);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass a = new MyClass();
+            a.field = 1;
+            a.Method();
+
+            Console.ReadKey();
+        }
+    }
+}
